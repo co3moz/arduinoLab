df28a724ece903554d5fc30ce4d76923169cc81e
co3moz <co3moz@gmail.com>
2014-12-09 04:19:55 +0200

    Fix

--- MultiThreading.h
+++ MultiThreading.h
@@ -1,3 +1,4 @@
 #define thread(name) unsigned long __time_ ## name = 0; void __function_ ## name ()
 #define use_thread(name) if(__time_ ## name < millis()) __function_ ## name ();
 #define delay_thread(name, time) __time_ ## name = millis() + time;
+
